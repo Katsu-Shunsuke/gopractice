b791821e8a42e5a38374b4ae33ad3d609cb1aba9
Katsu-Shunsuke <64359144+Katsu-Shunsuke@users.noreply.github.com>
2023-05-10 23:38:48 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var count int
+
+// go run main.go を実行してmain()から処理が始まる
+func main() {
+	http.HandleFunc("/countup", handler)
+	http.HandleFunc("/get", countHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	count++
+	fmt.Fprintln(w, count)
+}
+
+func countHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<html><h1>count</h1></html>")
+}
