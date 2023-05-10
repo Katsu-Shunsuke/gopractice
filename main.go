2ddd097835501b7711599352288eab5482b6ac3c
Katsu-Shunsuke <64359144+Katsu-Shunsuke@users.noreply.github.com>
2023-05-11 01:03:43 +0900

    modify

--- main.go
+++ main.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
-var count int
+var htmlStr string
 
 // go run main.go を実行してmain()から処理が始まる
 func main() {
 	fmt.Println("start")
+
+	http.HandleFunc("/getindexhtml", getindexHTML)
+
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/get", countHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	count++
-	fmt.Fprintln(w, count)
+	fmt.Fprintln(w, "<html><h1>Hello</h1></html>")
 }
 
-func countHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "<html><h1>count</h1></html>")
+func getindexHTML(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	htmlStr = string(data)
+
+	fmt.Fprintln(w, htmlStr)
 }
