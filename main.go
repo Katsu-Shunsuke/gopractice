f213bf101c69e0a3e15ec82f03ca6511a4c94dc3
Katsu-Shunsuke <64359144+Katsu-Shunsuke@users.noreply.github.com>
2023-05-11 14:32:05 +0900

    memo type

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 var htmlStr string
@@ -13,23 +15,39 @@ var htmlStr string
 func main() {
 	fmt.Println("start")
 
-	http.HandleFunc("/getindexhtml", getindexHTML)
-
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "<html><h1>Hello</h1></html>")
-}
-
-func getindexHTML(w http.ResponseWriter, r *http.Request) {
 	data, err := os.ReadFile("index.html")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	htmlStr = string(data)
 
+	http.HandleFunc("/", showScreen)
+	http.HandleFunc("/add_memo", addMemo)
+	http.ListenAndServe(":8080", nil)
+}
+
+func showScreen(w http.ResponseWriter, r *http.Request) {
+	/// fmt.Fprintln(w, "<html><h1>Hello</h1></html>")
 	fmt.Fprintln(w, htmlStr)
+}
+
+type Memo struct {
+	ID        string
+	Title     string
+	Body      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+var memos map[string]Memo
+
+// url -X POST -H "Content-Type: application/json" -d '{"ID":"tio"}' localhost:8080/add_memo
+func addMemo(w http.ResponseWriter, r *http.Request) {
+	m := &Memo{}
+	if err := json.NewDecoder(r.Body).Decode(m); err != nil {
+		fmt.Fprintln(w, err.Error())
+		return
+	}
+
+	fmt.Fprintln(w, m.ID)
 }
